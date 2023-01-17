549e536039ae443b90056d269b77e9fc127a57f1
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2023-01-17 13:53:13 +0300

    создал таску для подсчета cуммы всех подписок

--- service/services/models.py
+++ service/services/models.py
@@ -2,11 +2,22 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 from clients.models import Client
+from services.tasks import set_price
 
 
 class Service(models.Model):
     name = models.CharField(max_length=60)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, save_model=True, **kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Name: {self.name}'
@@ -23,11 +34,22 @@ class Plan(models.Model):
         MaxValueValidator(100)
     ])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args, save_model=True, **kwargs):
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return f'Discount : {self.plan_types} {self.discount_percent}%'
 
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
-    services = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
-    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
+    services = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
